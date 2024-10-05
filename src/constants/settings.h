c509acd8a90c9253464ff06fc2ae2794a08cf0c0
Reimund König <reimund@gmx.net>
2024-10-05 18:08:06 +0200

    add MQTT Data to settings

--- src/constants/settings.h
+++ src/constants/settings.h
@@ -1,6 +1,8 @@
 
 // Change this names to your need
-#define NAME "433_SmartHome"
+#define NAME "SmartHomeRF"
+
+// Used Pins
 #define REICEIVER_DATA 2   // D4
 #define REICEIVER_SLEEP 0  // D3
 #define TRANSMITTER_DATA 4 // D2
@@ -9,3 +11,7 @@
 #define EMPTY_03 12        // D6
 #define EMPTY_04 14        // D5
 #define EMPTY_05 2         //=> D3
+
+// MQTT Server configuration
+#define MQTT_SERVER_IP_ADDRESS "192.168.178.153"
+#define MQTT_SERVER_PORT 1883
