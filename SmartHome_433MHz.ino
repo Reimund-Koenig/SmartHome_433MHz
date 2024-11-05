358f63b536ae438535c616939f8bd7e53f9dbbc8
Reimund König <reimund@gmx.net>
2024-11-05 21:17:53 +0100

    add logger and heartbeat to ino

--- SmartHome_433MHz.ino
+++ SmartHome_433MHz.ino
@@ -1,11 +1,17 @@
+#include "src/logger.h"
 #include "src/mqttconnector.h"
 #include <RCSwitch.h>
+#ifdef DEBUG
+bool serial_enabled = true;
+#else
+bool serial_enabled = false;
+#endif
 
 // init 433MHz lib
 RCSwitch receiver = RCSwitch();
 RCSwitch transmitter = RCSwitch();
 uint32_t seconds;
-uint32_t last_seconds;
+uint32_t next_tick;
 unsigned long learning_code;
 
 WiFiClient espClient;
@@ -31,7 +37,13 @@ void mqtt_callback(char *topic, byte *payload, unsigned int length) {
 }
 
 void setup() {
-    // Serial.begin(115200);
+    if(serial_enabled) {
+        Serial.begin(19200);
+        while(!Serial) {
+            delay(1);
+        }
+    }
+    next_tick = 0;
     mqtt = new MQTTConnector(mqtt_callback);
     pinMode(REICEIVER_DATA, INPUT);
     pinMode(TRANSMITTER_DATA, OUTPUT);
@@ -40,10 +52,10 @@ void setup() {
     pinMode(REICEIVER_SLEEP, OUTPUT);
     receiver.enableReceive(REICEIVER_DATA);       // 433MHz Receiver
     transmitter.enableTransmit(TRANSMITTER_DATA); // 433MHz Sender
-    // Serial.println("Sniffer and sender for RF 433MHz");
     digitalWrite(TRANSMITTER_POWER, HIGH);
     digitalWrite(RECEIVER_POWER, HIGH);
     digitalWrite(REICEIVER_SLEEP, HIGH);
+    DEBUG_PRINTLN("Sniffer and sender for RF 433MHz");
 }
 
 bool isInList(int code, int bitLenght) {
@@ -67,53 +79,61 @@ void addToList(int code, int bitLenght) {
 
 void resend_known_codes() {
     for(int i = 0; i < numOfCodes; i++) {
-        // Serial.print("Send Code: ");
-        // Serial.print(codes[i].code);
-        // Serial.print(" / ");
-        // Serial.print(codes[i].bitLenght);
-        // Serial.println("bit");
+        DEBUG_PRINT("Send Code: ");
+        DEBUG_PRINT(codes[i].code);
+        DEBUG_PRINT(" / ");
+        DEBUG_PRINT(codes[i].bitLenght);
+        DEBUG_PRINTLN("bit");
         transmitter.send(codes[i].code, codes[i].bitLenght);
     }
 }
 
 void check_learning() {
     if(learning_code == 0) { return; }
-    // Serial.print("Start Learning Mode with Code: ");
-    // Serial.println(learning_code);
+    DEBUG_PRINT("Start Learning Mode with Code: ");
+    DEBUG_PRINTLN(learning_code);
     for(int k = 0; k < 10; k++) {
-        // Serial.println("Send Learning Code");
+        DEBUG_PRINTLN("Send Learning Code");
         transmitter.send(learning_code, 24);
         delay(10);
     }
-    // Serial.print("Finished Learning Mode with Code: ");
+    DEBUG_PRINT("Finished Learning Mode with Code: ");
     learning_code = 0;
 }
 
-void tick() {
-    if(seconds != last_seconds) {
-        // Serial.println(seconds);
-        last_seconds = seconds;
-        // resend_known_codes();
-    }
+void heartbeat() {
+    seconds = (int)(millis() / 1000);
+    if(seconds < next_tick) return;
+    mqtt->publish("home/433/heartbeat", "alive");
+    DEBUG_PRINTLN(seconds);
+    next_tick = seconds + 5;
+}
+
+void check_receiver() {
     if(!receiver.available()) { return; }
-    // Serial.print(seconds);
+    DEBUG_PRINT(seconds);
     unsigned long tmp_code = receiver.getReceivedValue();
     unsigned int tmp_bitLenght = receiver.getReceivedBitlength();
+    unsigned int protocol = receiver.getReceivedProtocol();
+    DEBUG_PRINT(" - Received: ");
+    DEBUG_PRINT(tmp_code);
+    DEBUG_PRINT(" / ");
+    DEBUG_PRINT(tmp_bitLenght);
+    DEBUG_PRINT("bit - ");
+    DEBUG_PRINT("Protocol: ");
+    DEBUG_PRINT(protocol);
+    DEBUG_PRINT(" - Add to List - ");
     addToList(tmp_code, tmp_bitLenght);
-    mqtt->publish_433("home/433", tmp_code);
-    // Serial.print("  - Received ");
-    // Serial.print(tmp_code);
-    // Serial.print(" / ");
-    // Serial.print(tmp_bitLenght);
-    // Serial.print("bit ");
-    // Serial.print("Protocol: ");
-    // Serial.println(receiver.getReceivedProtocol());
+    DEBUG_PRINT("MQTT Publish - ");
+    mqtt->publish_433(tmp_code);
+    DEBUG_PRINTLN(" - resetAvailable");
     receiver.resetAvailable();
 }
 
 void loop() {
-    seconds = (int)(millis() / 1000);
-    tick();
+    heartbeat();
+    check_receiver();
     check_learning();
     mqtt->loop();
+    delay(10);
 }
